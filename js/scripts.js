940656550172901ce345131f727ec8fc1cbf6954
proudcut <76070970+proudcut@users.noreply.github.com>
2021-02-04 14:23:13 -0500

    added font style and favicon. Changed title. Display pokemon list with height

--- js/scripts.js
+++ js/scripts.js
@@ -4,3 +4,11 @@ let pokemonList = [
   {name:'Squirtle', height:0.5, types:['water', 'monster']},
   {name:'Snorlax', height:2.1, types:['normal', 'monster']},
 ];
+
+for (let  i = 0; pokemonList.length; i++) {
+  if (pokemonList[i].height <= 2){
+    document.write("<br>" + pokemonList[i].name + " height is " + pokemonList[i].height + " m.");
+  }else if (pokemonList[i].height > 2){
+    document.write("<br>" + pokemonList[i].name + " height is " + pokemonList[i].height + " m." + " Wow, that's BIG!");
+  }
+}
