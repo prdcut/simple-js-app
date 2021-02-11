a055733121930529d6c02188d757bf5bfcd99217
proudcut <76070970+proudcut@users.noreply.github.com>
2021-02-11 10:16:41 -0500

    corrected the IIFE and forEach loop for correct display on the html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,24 @@
 //IIFE
 let pokemonRepository = (function () {
-// Aray of objects
-let pokemonList = [
-{name:'Charmander', height:0.6, types:['fire', 'dragon']},
-{name:'Mewtwo', height:2, types:['psychic', 'monster']},
-{name:'Squirtle', height:0.5, types:['water', 'monster']},
-{name:'Snorlax', height:2.1, types:['normal', 'monster']},
-];
+  let pokemonList = [
+  {name:'Charmander', height:0.6, types:['fire', 'dragon']},
+  {name:'Mewtwo', height:2, types:['psychic', 'monster']},
+  {name:'Squirtle', height:0.5, types:['water', 'monster']},
+  {name:'Snorlax', height:2.1, types:['normal', 'monster']},
+  ];
 
 return {
-add: function(pokemon) {
-pokemonList.push(pokemon);
-},
-getAll: function() {
-return pokemonList;
-}
-};
+    add: function(pokemon) {
+      pokemonList.push(pokemon);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
 })();
 
 // forEach Loop
-pokemonList.forEach(function(pokemon){
+pokemonRepository.getAll().forEach(function(pokemon){
   if (pokemon.height <= 2){
     document.write("<br>" + pokemon.name + "'s height is " + pokemon.height + " m.");
   } else if (pokemon.height > 2) {
